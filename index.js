0545d94e4b95611b71092364bfd519beddb1d231
Leksa <Surnacheva@ithub.ru>
2022-02-24 12:59:11 +0300

    Update process env

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const PORT = 3000;
+const PORT = process.env.PORT || 4011; // 4 - курс, 01 - pro, 1 - группа
 
 const application = express();
 
 application.get("/", function(request, response) {
-    response.send("<h1>Server is running!</h1>");
+    response.send(`
+        <h1>Server is running!</h1>
+        <h2>Hello ${process.env.NAME || "Guest"}</h2>
+    `);
 });
 
 application.listen(PORT);
