69cc44791d593b9ea70b32d181bd12d816b9b4e0
Leksa <Surnacheva@ithub.ru>
2022-03-03 13:16:41 +0300

    add db connection

--- api/api.js
+++ api/api.js
@@ -1,6 +1,21 @@
 const router = require("express").Router();
 
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: "3312",
+    user: "root",
+    password: "Qwerty123"
+});
+
 router.get("/", function(request, response) {
+    connection.connect(function(err) {
+        if (err) {
+            console.log("DB Danger!");
+        } else {
+            console.log("DB Success");
+        }
+    })
     response.send("<h1>API page</h1>");
 });
 
